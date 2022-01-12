0f7ce2498242484b896e7283609d2cc85e098c0b
Vahtee Boo <vahteeboo@gmail.com>
2022-01-12 12:33:13 +0200

    Part 6 of 11

---
+++ Moonshot2/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot2
+//
+//  Created by Vahtee Boo on 12.01.2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
